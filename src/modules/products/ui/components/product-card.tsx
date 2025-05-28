37ca467dc3d1a14154a5e95d9b0da0ba34bc73f2
Harsh Gupta <harshguptaaa17@gmail.com>
2025-05-28 18:57:49 +0530

    Product Page

--- src/modules/products/ui/components/product-card.tsx
+++ src/modules/products/ui/components/product-card.tsx
@@ -1,4 +1,4 @@
-import { generateTenantURL } from "@/lib/utils";
+import { formatCurrency, generateTenantURL } from "@/lib/utils";
 import { StarIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
@@ -25,17 +25,15 @@ export const ProductCard = ({
   reviewRating,
   imageUrl,
 }: ProductCardProps) => {
-
   const router = useRouter();
 
   const handleUserClick = (e: React.MouseEvent<HTMLDivElement>) => {
     e.preventDefault(),
-    e.stopPropagation(),
-
-    router.push(generateTenantURL(tenantSlug));
-  } 
+      e.stopPropagation(),
+      router.push(generateTenantURL(tenantSlug));
+  };
   return (
-    <Link href={`/products/${id}`}>
+    <Link href={`${generateTenantURL(tenantSlug)}/products/${id}`}>
       <div className="hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-shadow border rounded-md bg-white overflow-hidden h-full flex flex-col">
         <div className="relative aspect-square">
           <Image
@@ -70,13 +68,7 @@ export const ProductCard = ({
         </div>
         <div className="p-4">
           <div className="relative px-2 py-1 border bg-pink-400 w-fit">
-            <p className="text-sm font-medium">
-              {new Intl.NumberFormat("en-US", {
-                style: "currency",
-                currency: "USD",
-                maximumFractionDigits: 0,
-              }).format(Number(price))}
-            </p>
+            <p className="text-sm font-medium">{formatCurrency(price)}</p>
           </div>
         </div>
       </div>
@@ -85,7 +77,7 @@ export const ProductCard = ({
 };
 
 export const ProductCardSkeleton = () => {
-    return (
-        <div className="w-full aspect-3/4 bg-neutral-200 rounded-lg animate-pulse"></div>
-    )
-}
+  return (
+    <div className="w-full aspect-3/4 bg-neutral-200 rounded-lg animate-pulse"></div>
+  );
+};
