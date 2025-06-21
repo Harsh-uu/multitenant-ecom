36fd3781d9c0fb6a60716e68a6484efed4367f6e
Harsh Gupta <harshguptaaa17@gmail.com>
2025-06-22 03:10:52 +0530

    Fixes

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,21 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: (
+          process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000"
+        ).startsWith("https")
+          ? "https"
+          : "http",
+        hostname: new URL(
+          process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000"
+        ).hostname,
+      },
+    ],
+  },
   /* config options here */
 };
 
-export default withPayload(withPayload(withPayload(nextConfig)));
+export default withPayload(nextConfig);
